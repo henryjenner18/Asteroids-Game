3a8efadd5216ea7d145bc0398cee499b0ba7d23d
Henry Jenner <henryjenner18@gmail.com>
2017-09-27 17:07:22 +0100

    Comment added

--- core/src/main/AsteroidsMain.java
+++ core/src/main/AsteroidsMain.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.Game;
 import screens.GameScreen;
 
 public class AsteroidsMain extends Game { //Main class
-	
+	//a test comment
 	private static int width = 1600;
 	private static int height = 1000;
 
