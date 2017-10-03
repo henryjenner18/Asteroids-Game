eb65561f958b6108430fb1af242518d685adc109
henryjenner18 <32248187+henryjenner18@users.noreply.github.com>
2017-10-03 18:50:28 +0100

    Update DesktopLauncher.java

--- desktop/src/desktop/DesktopLauncher.java
+++ desktop/src/desktop/DesktopLauncher.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 import main.AsteroidsMain;
 
-public class DesktopLauncher { //
+public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Asteroids by Henry";
