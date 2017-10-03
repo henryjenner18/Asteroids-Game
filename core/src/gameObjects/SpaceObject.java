7eb7744cabc35a85f09ca5a826e1bfbdf26c0d12
henryjenner18 <32248187+henryjenner18@users.noreply.github.com>
2017-10-03 18:22:08 +0100

    Update SpaceObject.java

--- core/src/gameObjects/SpaceObject.java
+++ core/src/gameObjects/SpaceObject.java
@@ -7,9 +7,10 @@ public class SpaceObject {
 	protected Vector2 position;
 	protected Vector2 velocity;
 	protected int heading;
+	protected int r;
 	
-	protected float[] vertices;
-	protected int edges;
+	protected float[][] vertices;
+	protected int edges; // Also identical to number of vertices; vertices[0].length
 	
 	private float[] gradients; // Array for gradients of the edges
 	private float[] yIntercepts; // Array for y-intercepts of the edges
@@ -21,18 +22,17 @@ public class SpaceObject {
 		for(int i = 0; i < edges; i++) {
 			float x1, y1, x2, y2, m, c;
 			
-			x1 = vertices[i*2]; // Set x1 to i
-			y1 = vertices[(i*2)+1]; // y1 is the element after x1
+			x1 = vertices[i][0]; // Set x1 to i
+			y1 = vertices[i][1]; // y1 is the element after x1
 			
-			// Check to see if looking at the last vertex of the polygon
-			if(i * 2 == vertices.length - 2) {
+			if(i == edges - 1) { // Check to see if looking at the last vertex of the polygon
 				// Need to compare against the first vertex if we are currently checking the last vertex
-				x2 = vertices[0]; // Set x2 to the x value of the first vertex
-				y2 = vertices[1]; // Set x2 to the x value of the first vertex
+				x2 = vertices[0][0]; // Set x2 to the x value of the first vertex
+				y2 = vertices[0][1]; // Set x2 to the x value of the first vertex
 				
 			} else { // There are still vertices ahead of it to compare against in the array
-				x2 = vertices[(i*2)+2];
-				y2 = vertices[(i*2)+3];
+				x2 = vertices[i+1][0];
+				y2 = vertices[i+1][1];
 			}
 			
 			// Find m of each edge
@@ -49,7 +49,7 @@ public class SpaceObject {
 		}
 	}
 	
-	public float[] getVertices() {
+	public float[][] getVertices() {
 		return vertices;
 	}
 	
