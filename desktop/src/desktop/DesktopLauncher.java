73bc93d25ee003ccaee6b095d69b2bf6d9fb7afa
jennerh16 <jennerh16@DT-CSCI-001.sirisaac.net>
2018-01-16 10:13:48 +0000

    Removed fullscreen for screenshots

--- desktop/src/desktop/DesktopLauncher.java
+++ desktop/src/desktop/DesktopLauncher.java
@@ -11,12 +11,23 @@ public class DesktopLauncher {
 	private static int width, height;
 	
 	public static void main (String[] arg) {		
-		DisplayMode displayMode = LwjglApplicationConfiguration.getDesktopDisplayMode();
+		/*DisplayMode displayMode = LwjglApplicationConfiguration.getDesktopDisplayMode();
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.setFromDisplayMode(displayMode);
 		
 		width = displayMode.width;
-		height = displayMode.height;
+		height = displayMode.height;*/
+		
+		width = 1920;
+		height = 1080;
+		
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Asteroids by Henry";
+		config.width = width;
+		config.height = height;
+		config.fullscreen = false;
+		config.resizable = false;
+
 		
 		new LwjglApplication(new Main(width, height), config);
 	}
